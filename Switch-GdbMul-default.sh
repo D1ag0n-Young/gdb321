a7c383ca2965c417811fadd9a1f3667d9f96744c
1094093288 <1094093288@qq.com>
2021-10-27 09:12:45 +0800

    change SwitchGdbMuldefault.sh +x

--- Switch-GdbMul-default.sh
+++ Switch-GdbMul-default.sh
@@ -7,6 +7,7 @@ result=$(ls $path | grep "${str}")
 if [ "$result" != "" ];then
 		echo "#!/bin/sh" > $path$gdbmul
 		echo "exec gdb-multiarch -q -ex default-gdb-"$1' "$@"' >> $path$gdbmul
+		chmod +x $path$gdbmul
 		echo "success change default gdb-multiarch to $1!"
 else
     		echo "Please install gdb-multiarch first, run apt install gdb-multiarch !"
